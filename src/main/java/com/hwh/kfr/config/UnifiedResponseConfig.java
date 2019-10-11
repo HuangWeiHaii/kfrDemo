d35be36f945fa85132051bfe6055408287a69ced
hwh <1014755647@qq.com>
2019-10-11 10:16:49 +0800

    update

---
+++ src/main/java/com/hwh/kfr/config/UnifiedResponseConfig.java
@@ -0,0 +1,43 @@
+package com.hwh.kfr.config;
+
+import com.hwh.kfr.utils.CommonResult;
+import org.springframework.core.MethodParameter;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.server.ServerHttpRequest;
+import org.springframework.http.server.ServerHttpResponse;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice;
+
+/**
+ * @ description 配置统一响应体
+ * @ Date 2019年10月11日09:59:10
+ * @ author hwh
+ */
+@ControllerAdvice
+public class UnifiedResponseConfig implements ResponseBodyAdvice<Object>{
+    @Override
+    public boolean supports(MethodParameter methodParameter,
+                            Class<? extends HttpMessageConverter<?>> aClass) {
+        return methodParameter.hasMethodAnnotation(ResponseBody.class);
+    }
+
+    @Override
+    public Object beforeBodyWrite(Object body, MethodParameter methodParameter,
+                                  MediaType mediaType,
+                                  Class<? extends HttpMessageConverter<?>> aClass,
+                                  ServerHttpRequest serverHttpRequest,
+                                  ServerHttpResponse serverHttpResponse) {
+        if (body != null) {
+            return new CommonResult<Object>(body);
+        }
+        else {
+            CommonResult<Object> commonResult = new CommonResult<Object>(null);
+            commonResult.setStatus(-1);
+            commonResult.setErrorCode("500");
+            commonResult.setErrorMsg("error request");
+            return commonResult;
+        }
+    }
+}
